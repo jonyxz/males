5988fa1b6ff4aac059a571edd5dcae521dae2ce5
Johan Ridho <118349343+jonyxz@users.noreply.github.com>
2024-09-20 21:28:21 +0700

    Translate Bash Script
    Updated prompts and output messages in the Bash script to English for better accessibility.

--- males.sh
+++ males.sh
@@ -3,12 +3,12 @@
 echo "Name:"
 read NAME
 echo ""
-echo "Format (contoh: doc, jpg, mp4):"
+echo "Format (e.g., doc, jpg, mp4):"
 read FORMAT
 echo ""
-echo "Size (dalam bytes, contoh: 1024):"
+echo "Size (in bytes, e.g., 1024):"
 read SIZE
 echo ""
 
 dd if=/dev/urandom of="$NAME.$FORMAT" bs=1 count=$SIZE
-echo "File '$NAME.$FORMAT' sebesar $SIZE bytes telah dibuat."
+echo "File '$NAME.$FORMAT' of size $SIZE bytes has been created."
